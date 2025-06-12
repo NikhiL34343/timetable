edea63fe432aa1eedf0f229ee4832dacb03abe41
NikhiL34343 <ny9013815499@gmail.com>
2025-06-12 12:34:42 +0530

    app building fixes, app runs on android.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,11 +8,12 @@ plugins {
 android {
     namespace = "com.example.timetable_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -38,7 +39,15 @@ android {
         }
     }
 }
-
+dependencies {
+    implementation("androidx.core:core-ktx:1.10.1")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+}
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
 flutter {
     source = "../.."
 }
